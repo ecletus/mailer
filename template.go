5a46219415e2f1b8fb5cd78c2b9b77bfb8b7f7cd
Moises P. Sena <moisespsena@gmail.com>
2018-03-12 17:42:55 -0300

    Multisite support

--- template.go
+++ template.go
@@ -1,9 +1,8 @@
 package mailer
 
 import (
-	"html/template"
-	"net/http"
-	"net/url"
+	"github.com/moisespsena/template/html/template"
+	"github.com/qor/qor"
 )
 
 // Template email template
@@ -11,55 +10,51 @@ type Template struct {
 	Name    string
 	Layout  string
 	Data    interface{}
-	Request *http.Request
-	Writer  http.ResponseWriter
-	funcMap template.FuncMap
+	Context *qor.Context
+	funcs   *template.FuncValues
 }
 
 // Funcs set template's funcs
-func (tmpl Template) Funcs(funcMap template.FuncMap) Template {
-	tmpl.funcMap = funcMap
+func (tmpl Template) Funcs(funcMap... template.FuncMap) Template {
+	if tmpl.funcs == nil {
+		tmpl.funcs = &template.FuncValues{}
+	}
+	err := tmpl.funcs.Append(funcMap...)
+	if err != nil {
+		panic(err)
+	}
+	return tmpl
+}
+
+// FuncsValues set template's funcs
+func (tmpl Template) FuncsValues(funcValues... *template.FuncValues) Template {
+	tmpl.funcs.AppendValues(funcValues...)
 	return tmpl
 }
 
 // Render render template
 func (mailer Mailer) Render(t Template) Email {
 	var email Email
-
-	if t.funcMap == nil {
-		t.funcMap = template.FuncMap{}
-	}
-
-	if _, ok := t.funcMap["root_url"]; !ok {
-		t.funcMap["root_url"] = func() string {
-			if t.Request != nil && t.Request.URL != nil {
-				var newURL url.URL
-				newURL.Host = t.Request.URL.Host
-				newURL.Scheme = t.Request.URL.Scheme
-				return newURL.String()
-			}
-			return ""
-		}
-	}
+	var tmpl = mailer.Config.Render.Template().FuncValues(t.funcs)
 
 	if t.Layout != "" {
-		if result, err := mailer.Config.Render.Layout(t.Layout+".text").Funcs(t.funcMap).Render(t.Name+".text", t.Data, t.Request, t.Writer); err == nil {
+		if result, err := tmpl.Layout(t.Layout+".text").Render(t.Name+".text", t.Data, t.Context); err == nil {
 			email.Text = string(result)
 		}
 
-		if result, err := mailer.Config.Render.Layout(t.Layout+".html").Funcs(t.funcMap).Render(t.Name+".html", t.Data, t.Request, t.Writer); err == nil {
+		if result, err := tmpl.Layout(t.Layout+".html").Render(t.Name+".html", t.Data, t.Context); err == nil {
 			email.HTML = string(result)
-		} else if result, err := mailer.Config.Render.Layout(t.Layout).Funcs(t.funcMap).Render(t.Name, t.Data, t.Request, t.Writer); err == nil {
+		} else if result, err := tmpl.Layout(t.Layout).Render(t.Name, t.Data, t.Context); err == nil {
 			email.HTML = string(result)
 		}
 	} else {
-		if result, err := mailer.Config.Render.Funcs(t.funcMap).Render(t.Name+".text", t.Data, t.Request, t.Writer); err == nil {
+		if result, err := tmpl.Render(t.Name+".text", t.Data, t.Context); err == nil {
 			email.Text = string(result)
 		}
 
-		if result, err := mailer.Config.Render.Funcs(t.funcMap).Render(t.Name+".html", t.Data, t.Request, t.Writer); err == nil {
+		if result, err := tmpl.Render(t.Name+".html", t.Data, t.Context); err == nil {
 			email.HTML = string(result)
-		} else if result, err := mailer.Config.Render.Funcs(t.funcMap).Render(t.Name, t.Data, t.Request, t.Writer); err == nil {
+		} else if result, err := tmpl.Render(t.Name, t.Data, t.Context); err == nil {
 			email.HTML = string(result)
 		}
 	}
